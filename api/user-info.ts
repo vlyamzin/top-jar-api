c776f73f98f0b03b73558cc4ecf749aca1149d3f
Vlad Lyamzin <vlad.lyamzin@avenga.com>
2022-10-17 00:13:07 +0300

    add serverless function

---
+++ api/user-info.ts
@@ -0,0 +1,19 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+import { setup } from '../http-client';
+
+const token = process.env.MONO || '';
+const http = setup(token);
+
+export default async function (req: VercelRequest, res: VercelResponse) {
+    try {
+        const monoResponse = await http.get('/personal/client-info');
+        if (monoResponse.status !== 200) {
+            res.status(400).json({ message: monoResponse.statusText });
+            return;
+        }
+        res.status(200).json(monoResponse.data);
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ message: 'Something went wrong' });
+    }
+}
